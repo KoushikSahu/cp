ec7d67b1b7fbd897cbab630f360264cd60d4cd96
Koushik Sahu <koushiksahu68@gmail.com>
2021-08-27 23:47:17 +0530

    modify fb-template.cpp

--- facebook/hackercup2021/qualification/fb-template.cpp
+++ facebook/hackercup2021/qualification/fb-template.cpp
@@ -48,8 +48,8 @@ void testcase(){
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
-	int t;
-	cin>>t;
+	int t=1;
+	//cin>>t;
 	for(int i=1; i<=t; i++){
 		cout<<"Case #"<<i<<": ";
 		testcase();
