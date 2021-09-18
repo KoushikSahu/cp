333a83894e067ea0c94b355ac3ab8507b93cf4a4
Koushik Sahu <koushiksahu68@gmail.com>
2021-09-18 19:50:48 +0530

    codeforces/1336B.cpp

---
+++ codeforces/1527C.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define M_PI 3.14159265358979323846
+#define MOD 1000000007
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+typedef vector<int> vint;
+typedef vector<vint> vvint;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> ipair;
+typedef pair<ll, ll> llpair;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+const int nxm = 1e5+5;
+int t, n;
+array<ll, nxm> a;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    cin>>t;
+    while(t--){
+        cin>>n;
+        for(int i=0; i<n; i++) cin>>a[i];
+        map<int, ll> mp;
+        ll ans = 0;
+        for(int i=0; i<n; i++){
+            ans += mp[a[i]]*(n-i);
+            mp[a[i]] += (i+1);
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
+
