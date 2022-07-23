14b4a362766c728adfec840426775e99fe04533c
Koushik Sahu <koushiksahu68@gmail.com>
2022-07-23 23:33:25 +0530

    update and test tests

--- tests/compile_check.sh
+++ tests/compile_check.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-for file in `find . -name "*.cpp" -type f`
+set -e
+
+for file in `find . -not -path '*/\.*' -type f \( ! -iname ".*" \)`
 do
-	echo "compiling file $file"
-	g++ -std=c++17 $file
+	echo "Running test for $file"
+	if [[ $file == *.py ]]
+	then
+		python3 -m py_compile $file
+	elif [[ $file == *.cpp ]]
+	then
+		g++ $file
+	elif [[ $file == *.c ]]
+	then
+		gcc $file
+	elif [[ $file == *.md ]]
+	then
+		echo "Ignoring compile test for markdown files"
+	elif [[ $file == *Makefile ]]
+	then
+		echo "Ignoring compile test for makefile"
+	else
+		echo "Unknown file type"
+		exit 1
+	fi            
 done
 
