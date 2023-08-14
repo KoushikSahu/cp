519e01cf2baac31504098587eeb2cf7200412b5b
Koushik Sahu <koushiksahu68@gmail.com>
2023-08-15 00:27:09 +0530

    add cf practice problems

---
+++ codeforces/rustsrc/src/bin/1859A.rs
@@ -0,0 +1,92 @@
+use std::io::{stdin, stdout, BufWriter, prelude::BufRead, prelude::Write};
+use std::str;
+
+/*
+    Author: Koushik Sahu
+    Created: 11:45:34 PM(23:45:34) IST(+05:30) 13-08-2023 Sun
+*/
+
+fn solve<R: BufRead, W: Write>(sc: &mut Scanner<R>, wr: &mut W) {
+    let n: i32 = sc.next();
+    let mut a: Vec<i32> = (0..n).map(|_| sc.next()).collect();
+    a.sort();
+    let mx: i32 = a[n as usize-1];
+    let mut b: Vec<i32> = Vec::new();
+    let mut c: Vec<i32> = Vec::new();
+    if a[0] != mx {
+        for i in 0..n as usize {
+            if a[i] != mx {
+                b.push(a[i]);
+            } else {
+                c.push(a[i]);
+            }
+        }
+        writeln!(wr, "{} {}", b.len(), c.len());
+        for i in b.iter() {
+            write!(wr, "{} ", i);
+        }
+        writeln!(wr, "");
+        for i in c.iter() {
+            write!(wr, "{} ", i);
+        }
+        writeln!(wr);
+    } else {
+        writeln!(wr, "-1");
+    }
+}
+
+#[macro_export]
+macro_rules! dbg{
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(
+            concat!("{}:{}:{}: ",$(stringify!($a), " = {:?}, "),*),
+            file!(), line!(), column!(), $($a),*
+            );
+        #[cfg(not(debug_assertions))]
+        {};
+    }
+}
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn next<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = BufWriter::new(stdout.lock());
+    #[allow(unused_variables)]
+    let t: i32 = 1;
+    let t: i32 = scan.next(); 
+    for _ in 0..t {
+        solve(&mut scan, &mut out);
+    }
+}
