366b447f28bcca14097d1a17ac30bb4d10a42cd6
Koushik Sahu <koushiksahu68@gmail.com>
2023-08-27 20:00:29 +0530

    add rust practice problems

---
+++ atcoder/rustsrc/src/bin/ABC315_B.rs
@@ -0,0 +1,82 @@
+use std::io::{stdin, stdout, BufWriter, prelude::BufRead, prelude::Write};
+use std::str;
+
+/*
+    Author: Koushik Sahu
+    Created: 07:52:32 PM(19:52:32) IST(+05:30) 27-08-2023 Sun
+*/
+
+fn solve<R: BufRead, W: Write>(sc: &mut Scanner<R>, wr: &mut W) {
+    let m: i32 = sc.next();
+    let mut d: Vec<i32> = (0..m).map(|_| sc.next()).collect();
+    let mut mid: i32 = 0;
+    for i in 0..d.len() as usize {
+        mid += d[i];
+    }
+    mid += 1;
+    mid /= 2;
+    for i in 0..d.len() as usize {
+        if mid > d[i] {
+            mid -= d[i];
+        } else {
+            writeln!(wr, "{:} {:}", i+1, mid);
+            break;
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! dbg{
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(
+            concat!("{}:{}:{}: ",$(stringify!($a), " = {:?}, "),*),
+            file!(), line!(), column!(), $($a),*
+            );
+        #[cfg(not(debug_assertions))]
+        {};
+    }
+}
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn next<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = BufWriter::new(stdout.lock());
+    #[allow(unused_variables)]
+    let t: i32 = 1;
+    //let t: i32 = scan.next(); 
+    for _ in 0..t {
+        solve(&mut scan, &mut out);
+    }
+}
