4be7fcbdc9022af2e430539ea025df3f29ac7f83
Koushik Sahu <koushiksahu68@gmail.com>
2025-02-12 23:06:08 +0530

    add cc practice solutions

---
+++ codechef/rustsrc/src/bin/COOLSUB.rs
@@ -0,0 +1,98 @@
+use std::collections::BTreeSet;
+use std::io::{prelude::BufRead, prelude::Write, stdin, stdout, BufWriter};
+use std::str;
+
+/*
+    Author: Koushik Sahu
+    Created: 10:58:19 PM(22:58:19) IST(+05:30) 12-02-2025 Wed
+*/
+
+fn solve<R: BufRead, W: Write>(sc: &mut Scanner<R>, wr: &mut W) {
+    let n = sc.next::<usize>();
+    let a = (0..n).map(|_| sc.next::<i32>()).collect::<Vec<i32>>();
+    let mut s = BTreeSet::<i32>::new();
+    let mut done = false;
+    for i in a.iter() {
+        if s.contains(i) {
+            done = true;
+            writeln!(wr, "1").unwrap();
+            writeln!(wr, "{}", i).unwrap();
+            break;
+        } else {
+            s.insert(*i);
+        }
+    }
+    if !done {
+        writeln!(wr, "-1").unwrap();
+    }
+}
+
+#[macro_export]
+macro_rules! dbg{
+    ($($a:expr),*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(
+            concat!("{}:{}:{}: ",$(stringify!($a), " = {:?}, "),*),
+            file!(), line!(), column!(), $($a),*
+            );
+        #[cfg(not(debug_assertions))]
+        {};
+    }
+}
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+#[allow(dead_code)]
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn next<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+    fn try_next(&mut self) -> Option<String> {
+        loop {
+            self.buf_str.clear();
+            let bytes_read = self
+                .reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            if bytes_read == 0 {
+                return None; // End of file reached
+            }
+            return unsafe { Some(str::from_utf8_unchecked(&self.buf_str).trim().to_owned()) };
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = BufWriter::new(stdout.lock());
+    #[allow(unused_variables)]
+    let t: i32 = 1;
+    let t: i32 = scan.next();
+    for _ in 0..t {
+        solve(&mut scan, &mut out);
+    }
+}
